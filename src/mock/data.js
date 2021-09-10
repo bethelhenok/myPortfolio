dc7f07b84a6e282776214fd1cfe01f934c688a13
Sydney Stokes <sydneystokes@Sydneys-MacBook-Pro.local>
2021-09-10 17:54:14 -0400

    Customized

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,56 +2,47 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Bethel | Software Engineer', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my website', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hello my name is',
+  name: 'Bethel',
+  subtitle: "I'm a computer science student",
+  cta: 'Learn more',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne: 'Hey',
+  paragraphTwo: 'Beth',
+  paragraphThree: 'Me',
+  resume:
+    'https://docs.google.com/document/d/1aJ1w31Xv7BUw71qKaNGtvBTy497KfNF2/edit?usp=sharing&ouid=117610390018384376765&rtpof=true&sd=true', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'portfolio.png',
+    title: 'My Online Portfolio',
+    info: 'Online portfolio made using Gatsby',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: '/',
+    repo: 'https://github.com/bethelHenok/myPortfolio', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'communityCen.png',
+    title: 'Community Center Volunteering',
+    info: 'I volunteered here',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://www.montgomerycountymd.gov/rec/facilities/recreationcenters/praisner.html',
   },
 ];
 
@@ -59,7 +50,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'bethelhenok155@gmail.com',
 };
 
 // FOOTER DATA
@@ -67,28 +58,18 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'linkedin',
-      url: '',
+      name: 'instagram',
+      url: 'https://www.instagram.com/bethelhenok/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/bethelhenok',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
